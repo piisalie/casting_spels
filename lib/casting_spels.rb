b391e979a3e064b90810e382012480c20e4d22c4
Paul Dawson <excidere@gmail.com>
2013-04-02 18:33:58 -0500

    tying it together

---
+++ lib/casting_spels.rb
@@ -0,0 +1,14 @@
+require_relative 'casting_spels/bucket_item'
+require_relative 'casting_spels/chain_item'
+require_relative 'casting_spels/whiskey_bottle_item'
+require_relative 'casting_spels/wizard_item'
+#require_relative 'casting_spels/welder_item'
+#require_relative 'casting_spels/well_item'
+require_relative 'casting_spels/frog_item'
+
+require_relative 'casting_spels/room'
+require_relative 'casting_spels/living_room'
+require_relative 'casting_spels/attic_room'
+require_relative 'casting_spels/garden_room'
+
+require_relative 'casting_spels/player.rb'
