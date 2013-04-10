b13a0c6433b65eabd7192a820369f77e6ac45d61
Paul Dawson <excidere@gmail.com>
2013-04-10 08:01:47 -0500

    cleaning up a little

--- lib/casting_spels/chain_item.rb
+++ lib/casting_spels/chain_item.rb
@@ -1,19 +1,7 @@
 module CastingSpels
   class ChainItem
-    def initialize
-      @welded = false
-    end
-
-    def weld
-      @welded = true
-    end
-
     def inspect
-      if @welded
-        "a welded chain"
-      else
-        "an old chain"
-      end
+      "an old chain"
     end
 
     def to_s
